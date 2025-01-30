ee433d7649b4cffb264e4c07cfe11d51cd0113e8
Alex Turcan <alexandru.turcan@codefactorygroup.com>
2025-01-30 10:22:15 +0200

    Add UserController for retrieving current user information

---
+++ BookedIn.WebApi/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BookedIn.WebApi.Users;
+using BookedIn.WebApi.Auth;
+
+namespace BookedIn.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController(
+    IUserService userService,
+    ICurrentUserService currentUserService
+) : ControllerBase
+{
+    [HttpGet("me")]
+    public async Task<IActionResult> GetCurrentUser()
+    {
+        var email = currentUserService.GetUserEmail();
+        if (email == null)
+            return Unauthorized();
+
+        var user = await userService.GetUserByEmailAsync(email);
+        if (user == null)
+        {
+            return NotFound("User not found");
+        }
+
+        return Ok(user);
+    }
+}
