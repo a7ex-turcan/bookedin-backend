db4b491a4095a7b1eabaaf5112846862d6ff2f0b
Alex Turcan <alexandru.turcan@codefactorygroup.com>
2025-01-30 17:13:33 +0200

    Add user collections to UserController and create UserInfo model

---
+++ BookedIn.WebApi/Models/UserInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace BookedIn.WebApi.Models;
+
+public record UserInfo(
+    string Email,
+    string FullName,
+    DateTime DateOfBirth,
+    List<BookCollectionInfo> Collections
+);
+
+public record BookCollectionInfo(string CollectionName, List<string> WorkIds);
