0a7c87280c20a8677e4582939549a41b4d4e044c
Alex Turcan <alexandru.turcan@codefactorygroup.com>
2025-02-05 19:20:08 +0200

    Register SignUpRequestValidator in the service collection for dependency injection

--- BookedIn.WebApi/Users/Extensions/ServiceCollectionExtensions.cs
+++ BookedIn.WebApi/Users/Extensions/ServiceCollectionExtensions.cs
@@ -5,6 +5,8 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddUsers(this IServiceCollection services)
     {
         services.AddScoped<IUserService, UserService>();
+        services.AddScoped<SignUpRequestValidator>();
+
         return services;
     }
 }
